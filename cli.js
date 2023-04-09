39c5e254932e7991fcb595c99c2e02dae7c9acdc
Luisa <lorenitaa889@gmail.com>
2023-04-09 11:48:07 -0500

    mdLinks + cli completa

--- cli.js
+++ cli.js
@@ -9,50 +9,64 @@ const thrOption = process.argv[4]
 
 colors.setTheme({
     error: ['yellow', 'underline'],
-    mdError: ['cyan', 'italic'],
+    mdError: ['brightCyan', 'italic'],
     mdGuide: ['inverse','underline','italic'],
-    mdPath: ['cyan','underline']
+    mdPath: ['cyan','underline'],
+    mdCommand: ['brightGreen', 'bold']
 })
 
 function help(){
     console.log('\n---------------------------------->')
     console.log(' * A guide to mdLinks:\n'.mdGuide)
-    console.log(' >'.magenta+' To see basic info about a file, type: '+'Your file name'.bgMagenta.white +'\n')
-    console.log(' >'.magenta+' To know the status of file links, type: '+'Your file.md'.mdPath +' '+'--validate'.green +'\n')
-    console.log(' >'.magenta+' To see stats about file links, type: '+'Your file.md'.mdPath +' '+'--stats'.green +'\n')
-    console.log(' >'.magenta+' To see stats about links status, type: '+'Your file.md'.mdPath+' '+'--validate --stats '.green+ 'or '+ '--stats --validate'.green)
+    console.log(colors.brightRed(' Before you start: '+'Make sure you write your commands correctly!\n'))
+    console.log(' >'.magenta+' To see basic info about a file, type: '+'Your file name'.mdPath +'\n')
+    console.log(' >'.magenta+' To know the status of file links, type: '+'Your file.md'.mdPath +' '+'--validate'.mdCommand +'\n')
+    console.log(' >'.magenta+' To see stats about file links, type: '+'Your file.md'.mdPath +' '+'--stats'.mdCommand +'\n')
+    console.log(' >'.magenta+' To see stats about links status, type: '+'Your file.md'.mdPath+' '+'--validate --stats '.mdCommand+ 'or '+ '--stats --validate'.mdCommand)
     console.log('\n---------------------------------->')
 }
 
-
 function cli() {
-    if (secOption === '--validate' && thrOption === '--stats' || secOption === '--stats' && thrOption === '--validate') {
+     if (secOption === '--validate' && thrOption === '--stats' || secOption === '--stats' && thrOption === '--validate') {
         mdLinks(firOption, {validate: true}).then(content => statusRequest(content)
 
-        .then(res => console.log(statsArrLinks(res, 'href', true))))
+        .then(res => {
+           const statsValidated = statsArrLinks(res, 'href', true)
+           console.log('\n---------------------------------->'.yellow)
+           console.log(` Stats from ${firOption} \n`.mdPath)
+           console.log(` Total: ${statsValidated.Total}`)
+           console.log(` Unique: ${statsValidated.Unique}`)
+           console.log(` Broken: ${statsValidated.Broken}\n`)
+        }))
         .catch(err => console.log(`${err}`.error))
 
-    } else if (secOption === '--validate') {
+    } else if ( secOption === '--validate') {
         mdLinks(firOption, {validate: true}).then(content => statusRequest(content)
 
-        .then(console.log))
+        .then(res => console.log(colors.brightCyan(res))))
         .catch(err => console.log(`${err}`.error))
 
     } else if (secOption === '--stats') {
         mdLinks(firOption, {validate: false})
         
-        .then(content => console.log(statsArrLinks(content, 'href')))
+        .then(res => {
+            const stats = statsArrLinks(res, 'href', true)
+            console.log('\n---------------------------------->'.yellow)
+            console.log(` Stats from ${firOption} \n`.mdPath)
+            console.log(` Total: ${stats.Total}`)
+            console.log(` Unique: ${stats.Unique}\n`)
+         })
         .catch(err => console.log(`${err}`.error))
 
-    } else if (secOption === '--help') {
+    } else if (firOption === undefined) {
         help()
 
-    } else if (firOption === (undefined || '--help')) {
-        console.log('Md-Links Error: Please provide a path'.mdError)
+    } else if (firOption === '--help') {
+        help()
 
     } else {
         mdLinks(firOption, {validate: false})
-        .then(console.log)
+        .then(res => console.log(colors.brightCyan(res)))
         .catch(err => console.log(`${err}`.error))
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const {pathExistence, absOrRel, pathExtension, readFile, getFileLinks} = require('./api')
+const {pathExistence, absOrRel, pathExtension, readFile, getFileLinks, statusRequest, statsArrLinks} = require('./api')
 
-const mdLinks = (path, option) => {
+const mdLinks = (path, option, stats) => {
   return new Promise((resolve, reject) => {
     if (!pathExistence(path)) {
 
-      reject('Path does not exists')
+      reject('Path does not exists, please provide a valid path')
 
     } else if (pathExistence(path)){
 
@@ -15,15 +15,38 @@ const mdLinks = (path, option) => {
         
         readFile(absOrRel(path))
         .then(fileText => {
-          
+
+          if (option.validate === true) {
+            getFileLinks(fileText, absOrRel(path))
+            .then(links => {
+              if (stats === 'stats') {
+                statusRequest(links)
+
+                .then(result => resolve(statsArrLinks(result, 'href', true)))
+                .catch(err => err)
+              } else {
+                statusRequest(links)
+                
+                .then(result => resolve(result))
+                .catch(err => err)
+              }
+            })
+            .catch(nolink => reject(nolink)) 
+
+          } else if (option === 'stats') {
            getFileLinks(fileText, absOrRel(path))
-           .then(links => resolve(links))
+
+           .then(links => {
+            resolve(statsArrLinks(links, 'href'))
+           })
            .catch(nolink => reject(nolink))
+          }
+
         })
         .catch(noContent => reject(noContent))
       }
     }
   })
 }
-
+mdLinks('mdLinks.md', {validate: true}).then(console.log).catch(console.log)
 module.exports = mdLinks
